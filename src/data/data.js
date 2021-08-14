75fabbd9515ede10fb9dac01ff9d589164514fac
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-08-14 20:51:05 +0200

    repo list added

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,11 @@
 export const state = {
   isMenuOpen: false,
   userGitData: {},
+  frontEndProjects: [
+    'erkamguresen/Stopwatch',
+    'erkamguresen/my-to-do-list',
+    'erkamguresen/online-store',
+  ],
+  backEndProjects: [],
+  bookmarklets: ['erkamguresen/YouTube-Captions-Subtitles-Extractor'],
 };
