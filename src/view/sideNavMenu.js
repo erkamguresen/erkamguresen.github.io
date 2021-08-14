ec35c29f3e671639b6c07cd9acd90a7ee37ae1b6
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-08-14 15:24:57 +0200

    nav color varaibles

--- src/view/sideNavMenu.js
+++ src/view/sideNavMenu.js
@@ -3,18 +3,16 @@ export function openNav() {
 
   document.getElementById('sideBarContainer').style.width = '100%';
   document.getElementById('sideBarContainer').style.backgroundColor =
-    'rgba(0,0,0,0.4)';
+    'var(--dark-theme-disabled-color)';
 
-  document.getElementById('menu-button').style.color = '#818181';
-  //   document.getElementById('menu-button-container').style.left = '205px';
+  document.getElementById('menu-button').style.color =
+    'var(--dark-theme-base-color)';
 }
 
 export function closeNav() {
   document.getElementById('sideNav').style.width = '0';
   document.getElementById('sideBarContainer').style.width = '0';
-  document.getElementById('sideBarContainer').style.backgroundColor =
-    'rgba(0,0,0,0.4)';
 
-  document.getElementById('menu-button').style.color = '#000';
-  //   document.getElementById('menu-button-container').style.left = '10px';
+  document.getElementById('menu-button').style.color =
+    'var(--dark-theme-background-color)';
 }
