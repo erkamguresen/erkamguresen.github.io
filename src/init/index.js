efac274213d6842bf6c2085f6930e635706aedeb
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-02 19:27:03 +0200

    some project updates

--- src/init/index.js
+++ src/init/index.js
@@ -2,42 +2,81 @@ import '../listeners/sidenavListeners.js';
 import '../listeners/menuButtonListener.js';
 
 import { getUserData, getUserRepositories } from './gitHubData.js';
-import { state } from '../data/data.js';
-import { loadIntro } from '../view/intro-view.js';
+import { state, data } from '../data/data.js';
+
 import { renderFrontEndProjects } from '../view/frontEndProjects.js';
 import { renderBackEndProjects } from '../view/backEndProjects.js';
 import { renderBookmarkletProjects } from '../view/bookmarkletProjects.js';
 
-// loadIntro();
-
 state.userGitData = await getUserData();
 document.getElementById('avatar').src = state.userGitData.avatar_url;
 
-// state.projectsGitData.frontEndProjects = await getUserRepositories(
-//   state.frontEndProjects
-// );
+state.projectsGitData.bookmarklets = data.bookmarklets;
+state.projectsGitData.frontEndProjects = data.frontEndProjects;
+state.projectsGitData.backEndProjects = data.backEndProjects;
+
+let areResultsComplete = true;
+
+const resultBookmarklets = await getUserRepositories(state.bookmarklets);
+
+for (let i = 0; i < resultBookmarklets.length; i++) {
+  if (
+    !resultBookmarklets[i].incomplete_results === false ||
+    resultBookmarklets[i].incomplete_results === undefined
+  ) {
+    areResultsComplete = false;
+  }
+}
+
+if (areResultsComplete) {
+  state.projectsGitData.bookmarklets = resultBookmarklets;
+}
 
 const bookmarkletProjects = renderBookmarkletProjects();
 
 const bookmarkletContainer = document.getElementById('Bookmarklets');
 bookmarkletContainer.appendChild(bookmarkletProjects);
 
+areResultsComplete = true;
+
+const resultFrontEnd = await getUserRepositories(state.frontEndProjects);
+
+for (let i = 0; i < resultFrontEnd.length; i++) {
+  if (
+    !resultFrontEnd[i].incomplete_results === false ||
+    resultFrontEnd[i].incomplete_results === undefined
+  ) {
+    areResultsComplete = false;
+  }
+}
+if (areResultsComplete) {
+  state.projectsGitData.frontEndProjects = resultFrontEnd;
+}
+
 const frontEndProjects = renderFrontEndProjects();
 
 const frontEndContainer = document.getElementById('Front-End');
 frontEndContainer.appendChild(frontEndProjects);
 
-// state.projectsGitData.backEndProjects = await getUserRepositories(
-//   state.backEndProjects
-// );
+areResultsComplete = true;
+
+const resultBackend = await getUserRepositories(state.backEndProjects);
+
+for (let i = 0; i < resultBackend.length; i++) {
+  if (
+    !resultBackend[i].incomplete_results === false ||
+    resultBackend[i].incomplete_results === undefined
+  ) {
+    areResultsComplete = false;
+  }
+}
+if (areResultsComplete) {
+  state.projectsGitData.backEndProjects = resultBackend;
+}
 
 const backEndProjects = renderBackEndProjects();
 
 const backEndContainer = document.getElementById('Back-End');
 backEndContainer.appendChild(backEndProjects);
 
-// state.projectsGitData.bookmarklets = await getUserRepositories(
-//   state.bookmarklets
-// );
-
-console.log(state);
+// console.log('state', state);
